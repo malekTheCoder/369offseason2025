40beb029a3729ef2f02b7f07c1dac1dcea49c4ef
Malek Swilam <malekgswilam@gmail.com>
2025-07-12 15:57:46 -0400

    base for teleop to test differential system, servos too

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DifferentialTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DifferentialTest.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
-public class DifferentialTest {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp (name = "differential test")
+public class DifferentialTest extends OpMode {
+
+    private Servo left;
+    private Servo right;
+    private Servo claw;
+
+
+    @Override
+    public void init() {
+        left = hardwareMap.get(Servo.class, "left");
+        right = hardwareMap.get(Servo.class, "right");
+        claw = hardwareMap.get(Servo.class, "claw");
+
+
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.dpad_left){
+
+        }
+        else if (gamepad1.dpad_right){
+
+        }
+        else if (gamepad1.dpad_up){
+
+        }
+        else if (gamepad1.dpad_down){
+
+        }
+
+
+    }
 }
