bac5c2b2e086e5a9ad179b7758fa7efcee4d655b
Malek Swilam <malekgswilam@gmail.com>
2025-07-17 20:07:38 -0400

    added code to test the distance sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTest.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp
+public class DistanceSensorTest extends OpMode {
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    private DistanceSensor dist;
+
+
+    @Override
+    public void init() {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        dist = hardwareMap.get(DistanceSensor.class, "distance");
+
+    }
+
+    @Override
+    public void loop() {
+        handleDrivtrain();
+
+    }
+
+    private void handleDrivtrain() {
+        double lStickY = gamepad1.left_stick_y;
+        if ((lStickY > Math.abs(0.2)) && (dist.getDistance(DistanceUnit.INCH) > 12)){
+            frontLeft.setPower(0.5 * lStickY);
+            frontRight.setPower(0.5 * lStickY);
+            backLeft.setPower(0.5 * lStickY);
+            backRight.setPower(0.5 * lStickY);
+
+        }
+        else {
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
+        }
+    }
+}
